1ba97b98f924ffc409f16c5c1c1fe51f85aa402c
macbook <wnfid1040@gmail.com>
2022-08-01 18:30:59 +0900

    프로젝트 생성~!~!~!~!

---
+++ src/main/java/com/example/library/login/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.library.login.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+    private final Log log = LogFactory.getLog(LoginController.class);
+
+}
